7c457cc12016ca47fd52dde890b8f83ae4653438
Your Name <you@example.com>
2022-12-13 15:45:18 +0700

    commit

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -5,10 +5,30 @@ using namespace std;
 int main(){
 	
 	int i = 0, P[5000], k, result;
+	
 	while(i < 5000){
-		P[i] = 2*i+1;
+		P[i] = (2*i)+1;
 		i++;
 	}
-	
-	return 0;
-}
+
+	cout << "Please input k: " ;
+	cin >> k ;
+	if (k > 0){
+		i = 0 , result = 0 ;
+		while(i < 5000){
+			if(P[i]%k == 0){
+				result -= P[i] ;
+				i++ ;
+			}else{
+				result += P[i] ;
+				i++ ;
+				}
+			}
+		cout << "Result = "<< result;
+	}
+	else
+	{
+		cout << "Invalid input!!!" ;
+	}
+		return 0;
+	}
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
+int adiff(int A,int B){
+  int C;
+
+  if(A >= 360 || A <= -360){
+    A = A%360;
+  }
+
+  if(B >= 360 || B <= -360){
+    B = B%360;
+  }
+
+    C = abs(A-B);
+
+
+  if(abs(A-B) > 180){
+    C = 360 -abs(A-B);
+  }
+
+  return C ; 
+}
 
 int main(){
   cout << adiff(180,270);
